cdd455a292595568339c91695672aaac8f0b3e8c
Ja3Mamtora <jatinumamtora@gmail.com>
2022-12-23 23:01:11 +0530

    commit

---
+++ 974SubarraySumsDivisiblebyK.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int sum=0, rem=0;
+        int ans = 0;
+        unordered_map<int,int> mp;
+        mp[sum]++;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            rem = sum % k;
+            if(rem < 0) rem += k;
+            if(mp.find(rem) != mp.end())
+                ans += mp[rem];
+            mp[rem]++;
+        }
+        return ans;
+    }
+};
